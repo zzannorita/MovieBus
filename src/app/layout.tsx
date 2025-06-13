b24d1d76c925e6c74ef87f3da4059efdc5d01d39
zzannorita <tpals0518@naver.com>
2025-06-13 18:03:46 +0900

    Feat: React-Query 설정 및 뷰포트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./global.scss";
 import Header from "./components/Header/Header";
+
+const queryClient = new QueryClient();
+
 export default function RootLayout({
   children,
 }: {
@@ -7,9 +13,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>
-        <Header />
-        {children}
+        <QueryClientProvider client={queryClient}>
+          <Header />
+          {children}
+        </QueryClientProvider>
       </body>
     </html>
   );
